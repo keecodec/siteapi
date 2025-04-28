b5c4c573dd46c647b87429a3bffef7cef8c95681
keecodec <frkazeem@gmail.com>
2025-04-28 22:24:23 +0200

    Déploiement final avec chemins corrigés

---
+++ _next/static/6foFUbngYvmU95579eGlo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/960-edfba200e297815b.js","static/chunks/pages/index-cb6f40394eb0f174.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
